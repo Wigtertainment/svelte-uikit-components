d319a926926fdb1f16bb564924300316a152dc02
Egor Kin <info@egor-kin.com>
2023-07-07 09:29:09 +0300

    created text component

---
+++ src/lib/components/text/text.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import type {
+		FontColor,
+		FontSize,
+		FontStyle,
+		FontTransform,
+		FontWeight,
+	} from "./types.js";
+	export let style: FontStyle = undefined;
+	export let fontSize: FontSize = "default";
+	export let fontWeight: FontWeight = "normal";
+	export let italic: boolean = false;
+	export let transform: FontTransform = undefined;
+	export let color: FontColor = undefined;
+</script>
+
+<div
+	class={`${color ? "uk-text-" + color : ""}`}
+	class:uk-text-lead={style === "lead"}
+	class:uk-text-meta={style === "meta"}
+	class:uk-text-small={fontSize === "small"}
+	class:uk-text-default={fontSize === "default"}
+	class:uk-text-large={fontSize === "large"}
+	class:uk-text-light={fontWeight === "light"}
+	class:uk-text-normal={fontWeight === "normal"}
+	class:uk-text-bold={fontWeight === "bold"}
+	class:uk-text-lighter={fontWeight === "lighter"}
+	class:uk-text-bolder={fontWeight === "bolder"}
+	class:uk-text-italic={italic}
+	class:uk-text-capitalize={transform === "capitalize"}
+	class:uk-text-uppercase={transform === "uppercase"}
+	class:uk-text-lowercase={transform === "lowercase"}
+>
+	<slot />
+</div>
---
+++ src/lib/components/text/types.ts
@@ -0,0 +1,5 @@
+export type FontStyle = "lead" | "meta" | undefined;
+export type FontSize = "small" | "default" | "large";
+export type FontWeight = "light" | "normal" | "bold" | "lighter" | "bolder";
+export type FontTransform = "capitalize" | "uppercase" | "lowercase" | undefined;
+export type FontColor = "muted" | "emphasis" | "primary" | "secondary" | "success" | "warning" | "danger" | undefined;
